4aebc8520851972134e50f888b8ef6933169c03c
VladislavKondakov <agentvl@ukr.net>
2023-06-16 17:32:57 +0200

    e

---
+++ src/components/App.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchContacts } from 'reduce/operation';
+import { contactsReducer } from 'reduce/contactsSlice';
+import AddContactForm from './AddContactForm/AddContactForm';
+import ContactList from './ContactList/ContactList';
+
+const App = () => {
+  const contacts = useSelector((state) => state.contacts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      const parsedContacts = JSON.parse(savedContacts);
+      dispatch(fetchContacts(parsedContacts));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <div>
+      <AddContactForm />
+      <ContactList />
+    </div>
+  );
+};
+
+export default App;
